f5f8c9b2c1e5b0334cf48ec9ebd59a28f299bc94
Matheus Gaspechak Martins <mathgaspechak@outlook.com>
2017-03-19 14:05:32 -0400

    Novo padrão para banco de dados do firebase
    O novo padrão agora usa uma classe abstrata para reduzir a reescrita de
    código

---
+++ app/src/main/java/br/com/bunker/database/BaseDAO.java
@@ -0,0 +1,23 @@
+package br.com.bunker.database;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+abstract class BaseDAO<T> {
+
+    DatabaseReference db;
+    FirebaseUser currentUser;
+
+    BaseDAO() {
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        db = FirebaseDatabase.getInstance().getReference("bunker").child("users").child(currentUser.getUid());
+    }
+
+    public abstract String insert(T entity);
+
+    public abstract void update(T entity);
+
+    public abstract void remove(String key);
+}
