059a7ce45411b040cdea05cb2abf39f8a5fb6ce4
Matheus Gaspechak Martins <mathgaspechak@outlook.com>
2017-03-21 20:19:11 -0400

    Classe de inicialização da aplicação
    Uma classe da aplicação foi criada para habilitar a persistência offline
    com firebase

---
+++ app/src/main/java/br/com/bunker/Bunker.java
@@ -0,0 +1,14 @@
+package br.com.bunker;
+
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Bunker extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+    }
+}
