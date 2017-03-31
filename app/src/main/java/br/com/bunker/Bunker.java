d7bd910eb7125cee6b2035b0b0de0e6afdba07a4
Matheus Gaspechak Martins <mathgaspechak@outlook.com>
2017-03-31 00:40:48 -0400

    :sparkles: Import da biblioteca Hawk
    A biblioteca será utilizada como utilitário para salvar informações de
    forma segura

--- app/src/main/java/br/com/bunker/Bunker.java
+++ app/src/main/java/br/com/bunker/Bunker.java
@@ -3,6 +3,7 @@ package br.com.bunker;
 import android.app.Application;
 
 import com.google.firebase.database.FirebaseDatabase;
+import com.orhanobut.hawk.Hawk;
 
 public class Bunker extends Application {
 
@@ -10,5 +11,6 @@ public class Bunker extends Application {
     public void onCreate() {
         super.onCreate();
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        Hawk.init(this).build();
     }
 }
