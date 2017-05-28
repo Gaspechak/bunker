eea31e93ffba4bfa807b6d748f462c8ee94db050
Matheus Gaspechak <mathgaspechak@outlook.com>
2017-05-28 17:12:08 -0400

    Adicionar notificação de acesso ao App

--- app/src/main/java/br/com/bunker/Bunker.java
+++ app/src/main/java/br/com/bunker/Bunker.java
@@ -1,9 +1,19 @@
 package br.com.bunker;
 
 import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 
 import com.google.firebase.database.FirebaseDatabase;
 import com.orhanobut.hawk.Hawk;
+
+import br.com.bunker.view.MainActivity;
 
 public class Bunker extends Application {
 
@@ -12,6 +22,27 @@ public class Bunker extends Application {
         super.onCreate();
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         Hawk.init(this).build();
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_padlock)
+                        .setContentTitle("Bunker")
+                        .setContentText("Toque para abrir a seleção de senhas");
+
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            mBuilder.setPriority(Notification.PRIORITY_MIN);
+        }
+        mBuilder.setOngoing(true);
+
+        mNotificationManager.notify(15, mBuilder.build());
     }
 
     @Override
